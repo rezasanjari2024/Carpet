fd370ade0205c55306a7856dd6ab0d0387883feb
reza sanajri <sanjarireza1996@gmail.com>
2025-01-08 11:32:14 +0330

    Add video to page

--- js/myscript.js
+++ js/myscript.js
@@ -140,3 +140,15 @@ function Redirection(langue) {
   
   var redirectFA = document.getElementById('FA');
  redirectFA && redirectFA.addEventListener('click', () => Redirection('FA'));
+
+ document.getElementById("play-video-btn").addEventListener("click", function() {
+    const videoContainer = document.getElementById("video-container");
+    const video = document.getElementById("about-video");
+
+    // Toggle video visibility
+    if (videoContainer.style.display === "none") {
+        videoContainer.style.display = "block";
+        video.play();
+        this.style.display = "none"; // Hide the button after click
+    }
+});
